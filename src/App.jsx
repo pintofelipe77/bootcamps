4248dd5a6ffbaa5177227472cccfe1762cfa7b03
pintofelipe77 <developerpinto77@gmail.com>
2024-09-10 21:41:04 -0500

    add proyect the react calculator

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import DigitButton from "./components/DigitButton";
+
+function App() {
+
+  return (
+    <>
+      <DigitButton message="Hola" message1= "hola1" message2 = "hola2"/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/DigitButton.jsx
@@ -0,0 +1,13 @@
+function DigitButton(props){
+
+    return(
+    <>
+        <h1>{props.message}</h1>
+        <h1>{props.message1}</h1>
+        <h1>{props.message2}</h1>
+    </>
+
+    )
+}
+
+export default DigitButton;
